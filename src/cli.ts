a9fbf0f21ee14cadd1b12a97ab83e5854a11eb06
Misha Balin <m.balin@icloud.com>
2020-07-01 14:42:12 +0300

    parse arguments and handling input

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,36 @@
-export function run(args: string[]) {
-  console.log([...args]);
+import arg from 'arg';
+import promptForMissingOptions from './promptForMissingOptions';
+
+function parseArgumentsIntoOptions(rawArgs: string[]) {
+  const args = arg(
+    {
+      '--update': Boolean,
+      '--git': Boolean,
+      '--ssh': Boolean,
+      '--install': Boolean,
+      '--yes': Boolean,
+      '-u': '--update',
+      '-g': '--git',
+      '-i': '--install',
+      '-y': '--yes',
+    },
+    {
+      argv: rawArgs.slice(2),
+    },
+  );
+  return {
+    update: args['--update'] || false,
+    git: args['--git'] || false,
+    ssh: args['--ssh'] || false,
+    runInstall: args['--install'] || false,
+    skipPrompts: args['--yes'] || false,
+  };
+}
+
+export type TOptions = ReturnType<typeof parseArgumentsIntoOptions>;
+
+export async function run(args: string[]) {
+  const rawOptions = parseArgumentsIntoOptions(args);
+  const options = await promptForMissingOptions(rawOptions);
+  console.log(options);
 }
---
+++ src/promptForMissingOptions.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import inquirer from 'inquirer';
+import { TOptions } from './cli';
+
+export default async function promptForMissingOptions(options: TOptions) {
+  if (options.skipPrompts) return options;
+
+  const questions = [];
+
+  if (!options.update) {
+    questions.push({
+      type: 'confirm',
+      name: 'update',
+      message: 'Update system?',
+      default: true,
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Configure git?',
+      default: true,
+    });
+  }
+
+  if (!options.ssh) {
+    questions.push({
+      type: 'confirm',
+      name: 'ssh',
+      message: 'Configure ssh?',
+      default: true,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+
+  return {
+    ...options,
+    update: options.update || answers.update,
+    git: options.git || answers.git,
+    ssh: options.ssh || answers.ssh,
+  };
+}
