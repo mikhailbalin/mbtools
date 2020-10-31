45abcdacb6cddbd10760c217d55828ee6491bc22
Misha Balin <m.balin@icloud.com>
2020-10-31 23:48:05 +0300

    fix system update

--- src/commands/updateSystem.ts
+++ src/commands/updateSystem.ts
@@ -1,6 +1,7 @@
 import execAsRoot from '../utils/execAsRoot';
 
 export async function updateSystem(password: string) {
-  const responce = await execAsRoot('apt-get update', password);
-  return responce;
+  const apt = 'apt-get';
+  await execAsRoot(`${apt} update`, password);
+  await execAsRoot(`${apt} upgrade -y`, password);
 }
