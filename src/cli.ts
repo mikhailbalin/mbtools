bd3e3fa5534140fda0cd22294c27eb59c57ff1fc
Misha Balin <m.balin@icloud.com>
2020-10-31 12:40:29 +0300

    update dotenv setup

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,6 @@
-import dotenv from 'dotenv';
-import parseArgumentsIntoOptions from './utils/parseArgumentsIntoOptions';
-import promptForMissingOptions from './utils/promptForMissingOptions';
+import './dotenvSetup';
+import { parseArgumentsIntoOptions, promptForMissingOptions } from './utils';
 import { setupSystem } from './main';
-
-dotenv.config();
 
 export async function run(args: string[]) {
   const rawOptions = parseArgumentsIntoOptions(args);
