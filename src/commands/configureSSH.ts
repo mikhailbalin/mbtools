2f14624ab6affd1a8a3707b4b08c06dd6f5a16cc
Misha Balin <m.balin@icloud.com>
2020-10-31 16:01:36 +0300

    add shh config

--- src/commands/configureSSH.ts
+++ src/commands/configureSSH.ts
@@ -1,13 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import os from 'os';
+import path from 'path';
+import { writeAsync } from 'fs-jetpack';
 import { Dropbox } from 'dropbox';
 import fetch from 'node-fetch';
+import execa from 'execa';
 
 const dbx = new Dropbox({
   accessToken: process.env.DROPBOX_ACCESS_TOKEN,
   fetch,
 });
 
-export function configureSSH() {
-  // dbx.filesListFolder({ path: '' }).then((res) => console.log(res));
-  console.log(process.env.DROPBOX_ACCESS_TOKEN);
+export async function configureSSH() {
+  try {
+    const response: any = await dbx.filesListFolder({ path: '/ssh' });
+    const sshFolderPath = path.join(os.homedir(), '.ssh');
+
+    for (const file of response.result.entries) {
+      const data: any = await dbx.filesDownload({ path: file.path_lower });
+      const filePath = path.join(sshFolderPath, data.result.name);
+      await writeAsync(filePath, data.result.fileBinary);
+      await execa.command(`chmod 600 ${filePath}`);
+    }
+
+    await execa.command(`chmod 700 ${sshFolderPath}`);
+  } catch (error) {
+    throw new Error('SSH config');
+  }
 }
