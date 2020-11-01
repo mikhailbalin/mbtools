90a404844c02f07d7f0cbcc3e4e301c4686b3120
Misha Balin <m.balin@icloud.com>
2020-11-01 10:27:38 +0300

    add git try catch

--- src/commands/configureGit.ts
+++ src/commands/configureGit.ts
@@ -1,13 +1,17 @@
 import execa from 'execa';
 
 export async function configureGit() {
-  const settings = new Map([
-    ['user.name', 'Misha Balin'],
-    ['user.email', 'm.balin@icloud.com'],
-    ['core.editor', 'nano'],
-  ]);
+  try {
+    const settings = new Map([
+      ['user.name', 'Misha Balin'],
+      ['user.email', 'm.balin@icloud.com'],
+      ['core.editor', 'nano'],
+    ]);
 
-  for (const [k, v] of settings.entries()) {
-    await execa('git', ['config', '--global', k, v]);
+    for (const [k, v] of settings.entries()) {
+      await execa('git', ['config', '--global', k, v]);
+    }
+  } catch {
+    throw new Error('Git initialization');
   }
 }
