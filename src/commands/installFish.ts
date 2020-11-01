4ca651f854a3e0c8b4d15366066106a4f70d0859
Misha Balin <m.balin@icloud.com>
2020-11-01 10:24:24 +0300

    add fish try catch

--- src/commands/installFish.ts
+++ src/commands/installFish.ts
@@ -7,39 +7,43 @@ export async function installFish(
   task: Listr.ListrTaskWrapper,
   options: Omit<TOptions, 'skipPrompts'>,
 ) {
-  const isInstalled = await checkInstalled('fish -v');
+  try {
+    const isInstalled = await checkInstalled('fish -v');
 
-  if (!isInstalled) {
-    task.output = 'Installing...';
+    if (!isInstalled) {
+      task.output = 'Installing...';
 
-    const commands = [
-      'apt-add-repository ppa:fish-shell/release-3',
-      'apt-get update',
-      'apt-get install fish -y',
-    ];
+      const commands = [
+        'apt-add-repository ppa:fish-shell/release-3',
+        'apt-get update',
+        'apt-get install fish -y',
+      ];
 
-    for (const command of commands) {
-      await execAsRoot(command, options.password!);
+      for (const command of commands) {
+        await execAsRoot(command, options.password!);
+      }
     }
+
+    task.output = 'Setting config...';
+
+    await setFishConfig('config', options);
+    await setFishConfig('fish_prompt', options);
+
+    if (isInstalled) return;
+
+    const defaultShellProcess = execa.command('chsh -s /usr/bin/fish');
+
+    defaultShellProcess.stderr?.on('data', (data) => {
+      const chunk = data.toString('utf8');
+      if (chunk.includes('Password')) {
+        defaultShellProcess.stdin?.write(`${options.password}\n`);
+      }
+    });
+
+    await defaultShellProcess;
+
+    await execa.command('set fish_greeting', { shell: 'fish' });
+  } catch {
+    throw new Error('Fish config');
   }
-
-  task.output = 'Setting config...';
-
-  await setFishConfig('config', options);
-  await setFishConfig('fish_prompt', options);
-
-  if (isInstalled) return;
-
-  const defaultShellProcess = execa.command('chsh -s /usr/bin/fish');
-
-  defaultShellProcess.stderr?.on('data', (data) => {
-    const chunk = data.toString('utf8');
-    if (chunk.includes('Password')) {
-      defaultShellProcess.stdin?.write(`${options.password}\n`);
-    }
-  });
-
-  await defaultShellProcess;
-
-  await execa.command('set fish_greeting', { shell: 'fish' });
 }
