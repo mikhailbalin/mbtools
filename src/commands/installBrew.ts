40564d9dfcc1855e3dd104c64841fe8160268e60
Misha Balin <m.balin@icloud.com>
2020-11-01 10:31:07 +0300

    add brew try catch

--- src/commands/installBrew.ts
+++ src/commands/installBrew.ts
@@ -4,22 +4,28 @@ import { execAsRoot } from '../utils';
 import { getConfigPath } from '../utils';
 
 export async function installBrew(password: string) {
-  await execAsRoot(
-    'apt-get install build-essential curl file git -y',
-    password,
-  );
-  await execa.command(
-    'git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew',
-  );
-  await execa.command('mkdir ~/.linuxbrew/bin');
-  await execa.command('ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin');
-
-  const result = await existsAsync(getConfigPath('fish.config'));
-  if (result === 'file') {
-    await appendAsync(
-      result,
-      '\n# Brew\neval (~/.linuxbrew/bin/brew shellenv)',
+  try {
+    await execAsRoot(
+      'apt-get install build-essential curl file git -y',
+      password,
     );
-  } else {
+    await execa.command(
+      'git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew',
+    );
+    await execa.command('mkdir ~/.linuxbrew/bin');
+    await execa.command(
+      'ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin',
+    );
+
+    const result = await existsAsync(getConfigPath('fish.config'));
+    if (result === 'file') {
+      await appendAsync(
+        result,
+        '\n# Brew\neval (~/.linuxbrew/bin/brew shellenv)',
+      );
+    } else {
+    }
+  } catch {
+    throw new Error('Brew install');
   }
 }
