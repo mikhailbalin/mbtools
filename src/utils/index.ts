f9a4fff14c02de43efa41f107d1be48c3306b019
Misha Balin <m.balin@icloud.com>
2020-11-01 10:20:35 +0300

    remove copy templates

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export { default as checkInstalled } from './checkInstalled';
-export { default as copyTemplateFiles } from './copyTemplateFiles';
 export { default as execAsRoot } from './execAsRoot';
 export { default as parseArgumentsIntoOptions } from './parseArgumentsIntoOptions';
 export { default as promptForMissingOptions } from './promptForMissingOptions';
