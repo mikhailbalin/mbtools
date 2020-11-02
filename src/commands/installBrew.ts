89d4e0f1517da32d1bf0233e1eacc0779f40cfce
Misha Balin <m.balin@icloud.com>
2020-11-02 23:56:59 +0300

    fix brew

--- src/commands/installBrew.ts
+++ src/commands/installBrew.ts
@@ -1,4 +1,5 @@
 import execa from 'execa';
+import os from 'os';
 import { ListrTaskWrapper } from 'listr';
 import { TContext } from '../types';
 import { execAsRoot } from '../utils';
@@ -12,11 +13,11 @@ export async function installBrew(
   try {
     await execAsRoot('apt-get install build-essential -y', password);
     await execa.command(
-      'git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew',
+      `git clone https://github.com/Homebrew/brew ${os.homedir()}/.linuxbrew/Homebrew`,
     );
-    await execa.command('mkdir ~/.linuxbrew/bin');
+    await execa.command(`mkdir ${os.homedir()}/.linuxbrew/bin`);
     await execa.command(
-      'ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin',
+      `ln -s ${os.homedir()}/.linuxbrew/Homebrew/bin/brew ${os.homedir()}/.linuxbrew/bin`,
     );
 
     setFishConfig('config', { ...ctx, brew: true });
