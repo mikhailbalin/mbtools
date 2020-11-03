f46e3eb3d8d272d8e82e7902a288a341d3d9226b
Misha Balin <m.balin@icloud.com>
2020-11-03 19:21:04 +0300

    update makeDefaultShell errors

--- src/commands/installFish/makeDefaultShell.ts
+++ src/commands/installFish/makeDefaultShell.ts
@@ -1,14 +1,18 @@
 import execa from 'execa';
 
 export const makeDefaultShell = async (password: string) => {
-  const defaultShellProcess = execa.command('chsh -s /usr/bin/fish');
+  try {
+    const defaultShellProcess = execa.command('chsh -s /usr/bin/fish');
 
-  defaultShellProcess.stderr?.on('data', (data) => {
-    const chunk = data.toString('utf8');
-    if (chunk.includes('Password')) {
-      defaultShellProcess.stdin?.write(`${password}\n`);
-    }
-  });
+    defaultShellProcess.stderr?.on('data', (data) => {
+      const chunk = data.toString('utf8');
+      if (chunk.includes('Password')) {
+        defaultShellProcess.stdin?.write(`${password}\n`);
+      }
+    });
 
-  await defaultShellProcess;
+    await defaultShellProcess;
+  } catch {
+    throw new Error('Error making fish default');
+  }
 };
