9ff6ffd90bdeededa3e6cc841332f80bd6470938
Misha Balin <m.balin@icloud.com>
2020-11-03 19:21:25 +0300

    add setupConfigs task

---
+++ src/commands/installFish/setupConfigs.ts
@@ -0,0 +1,20 @@
+import { ListrTaskWrapper } from 'listr';
+import execa from 'execa';
+import type { TContext } from '../../types';
+import { makeDefaultShell } from './makeDefaultShell';
+import setFishConfig from './setFishConfig';
+
+export async function setupConfigs(
+  ctx: TContext,
+  task: ListrTaskWrapper,
+  password: string,
+) {
+  try {
+    await setFishConfig('config', ctx);
+    await setFishConfig('fish_prompt', ctx);
+    await makeDefaultShell(password);
+    await execa.command('set fish_greeting', { shell: 'fish' });
+  } catch {
+    task.skip('Error setting fish configs');
+  }
+}
