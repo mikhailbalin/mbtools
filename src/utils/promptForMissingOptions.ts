6a53218e9bea6b6b2c3228e112eefce41897b613
Misha Balin <m.balin@icloud.com>
2020-11-04 09:35:42 +0300

    refactor prompt main options

--- src/utils/promptForMissingOptions.ts
+++ src/utils/promptForMissingOptions.ts
@@ -1,14 +1,7 @@
 import inquirer from 'inquirer';
-import {
-  READY,
-  ARG_BREW,
-  ARG_FISH,
-  ARG_GIT,
-  ARG_SSH,
-  ARG_UPDATE,
-} from '../constants';
+import { ARG_BREW, ARG_FISH, ARG_GIT, ARG_SSH, ARG_UPDATE } from '../constants';
 import type { TOptions } from '../types';
-import { config } from '../config';
+import { promptMainOptions } from './prompts/promptMainOptions';
 
 const includesAny = <T>(arr: T[], values: T[]) =>
   values.some((v) => arr.includes(v));
@@ -21,60 +14,12 @@ const passwordValidator = async (input: string) => {
 export default async function promptForMissingOptions(
   options: TOptions,
 ): Promise<Omit<TOptions, 'skipPrompts'>> {
-  const {
-    update,
-    git,
-    ssh,
-    fish,
-    brew,
-    password: optionsPassword,
-    skipPrompts,
-  } = options;
+  const { password: optionsPassword, skipPrompts, ...rest } = options;
 
-  let answers: string[] | null = null;
+  const answers: string[] | null = skipPrompts
+    ? null
+    : await promptMainOptions(rest);
   let password: string | null = optionsPassword;
-
-  if (!skipPrompts) {
-    const questions = [
-      {
-        type: 'checkbox',
-        name: 'actions',
-        message: 'What would you like to do?',
-        choices: [
-          {
-            name: 'Update system',
-            value: ARG_UPDATE,
-            checked: update,
-          },
-          {
-            name: 'Configure git',
-            value: ARG_GIT,
-            checked: git,
-            disabled: config.get(ARG_GIT) && READY,
-          },
-          {
-            name: 'Configure or install fish',
-            value: ARG_FISH,
-            checked: fish,
-          },
-          {
-            name: 'Configure ssh',
-            value: ARG_SSH,
-            checked: ssh,
-            disabled: config.get(ARG_SSH) && READY,
-          },
-          {
-            name: 'Install Brew',
-            value: ARG_BREW,
-            checked: brew,
-          },
-        ],
-      },
-    ] as const;
-
-    const resault = await inquirer.prompt(questions);
-    answers = resault.actions;
-  }
 
   const shouldAskPassword =
     skipPrompts ||
---
+++ src/utils/prompts/promptMainOptions.ts
@@ -0,0 +1,59 @@
+import inquirer from 'inquirer';
+import {
+  READY,
+  ARG_BREW,
+  ARG_FISH,
+  ARG_GIT,
+  ARG_SSH,
+  ARG_UPDATE,
+} from '../../constants';
+import { config } from '../../config';
+import { TOptions } from '../../types';
+
+export const promptMainOptions = async ({
+  update,
+  git,
+  fish,
+  ssh,
+  brew,
+}: Omit<TOptions, 'skipPrompts' | 'password'>) => {
+  const questions = [
+    {
+      type: 'checkbox',
+      name: 'actions',
+      message: 'What would you like to do?',
+      choices: [
+        {
+          name: 'Update system',
+          value: ARG_UPDATE,
+          checked: update,
+        },
+        {
+          name: 'Configure git',
+          value: ARG_GIT,
+          checked: git,
+          disabled: config.get(ARG_GIT) && READY,
+        },
+        {
+          name: 'Configure or install fish',
+          value: ARG_FISH,
+          checked: fish,
+        },
+        {
+          name: 'Configure ssh',
+          value: ARG_SSH,
+          checked: ssh,
+          disabled: config.get(ARG_SSH) && READY,
+        },
+        {
+          name: 'Install Brew',
+          value: ARG_BREW,
+          checked: brew,
+        },
+      ],
+    },
+  ] as const;
+
+  const resault = await inquirer.prompt(questions);
+  return resault.actions;
+};
