6b0f8cedc38c09ffd1346ef24254ad7ff7550592
Misha Balin <m.balin@icloud.com>
2020-11-07 07:02:30 +0300

    update types

--- src/types.ts
+++ src/types.ts
@@ -9,10 +9,29 @@ export interface IArguments {
 }
 
 export type TContext = Pick<IArguments, 'git' | 'ssh' | 'fish'> & {
-  display: boolean;
   brew:
     | {
-        yarn: boolean;
+        node: boolean;
+        yarn:
+          | {
+              serve: boolean;
+              lerna: boolean;
+              pkg: boolean;
+              uuid: boolean;
+              '@vue/cli': boolean;
+              'json-server': boolean;
+              'gatsby-cli': boolean;
+              'npm-check-updates': boolean;
+              'package-size': boolean;
+            }
+          | false;
+        php: boolean;
+        imagemagick: boolean;
+        ffmpeg: boolean;
+        'azure-cli': boolean;
+        'git-lfs': boolean;
+        'git-flow-avh': boolean;
       }
     | false;
+  display: boolean;
 };
