b659d6cf6d1ae027ff3187e9db4dce697735aac3
Misha Balin <m.balin@icloud.com>
2020-11-07 08:58:36 +0300

    u

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 export { default as execAsRoot } from './execAsRoot';
 export { default as parseArgumentsIntoOptions } from './parseArgumentsIntoOptions';
-export { default as promptForMissingOptions } from './prompts/promptForMissingOptions';
+export { default as promptMissingOptions } from './prompts/promptMissingOptions';
